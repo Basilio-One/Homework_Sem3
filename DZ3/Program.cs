e7950ad13f4c46730dfefca9c8336824852edaf3
Basilio-one <vasya-w@yandex.ru>
2022-12-06 13:21:48 +0300

    Zadacha 3 tablica kubov

---
+++ DZ3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат чисел равен: ");
+
+if (N<0) N=-N;
+else if (N==0) Console.WriteLine("0");
+for (int i = 1; i <= N; i++)
+{
+    int sqrt = (i * i * i);
+    Console.Write($"{sqrt} ");
+}
